3d65a3e861c7aa66d84062c9465360f2592bc014
new <Buihieu00528@gmail.com>
2023-12-10 14:08:44 +0700

    new web

--- src/compoment/Scroll/Scroll.js
+++
@@ -1,5 +0,0 @@
-function Scroll() {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-    return (<></>)
-  }
-  export default Scroll
--- src/compoment/gallery/gallery.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function gallery() {
-  return (
-    <div>gallery</div>
-  )
-}
-
-export default gallery
